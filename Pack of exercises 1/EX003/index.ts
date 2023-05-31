c7f7fe63fce79ba8dbe57834022b0b28b02958c7
Nathan Mateus <117992913+onathanmateus@users.noreply.github.com>
2023-05-31 16:24:36 -0300

    Eu criei a pasta EX003

---
+++ Pack of exercises 1/EX003/index.ts
@@ -0,0 +1,6 @@
+function passCordinates(cord: { x: number; y: number }) {
+  console.log("X cordinates: " + cord.x);
+  console.log("Y cordinates: " + cord.y);
+}
+const objCord = { x: 323, y: 623 };
+passCordinates(objCord);
