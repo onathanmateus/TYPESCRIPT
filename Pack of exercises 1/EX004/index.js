7d71fe1d6dc9ae79865dab3f05452143b6f7b2b8
Nathan Mateus <117992913+onathanmateus@users.noreply.github.com>
2023-06-02 16:43:55 -0300

    Eu criei a pasta EX004

---
+++ Pack of exercises 1/EX004/index.js
@@ -0,0 +1,6 @@
+function showNumber(a, b, c) {
+    console.log("A: " + a);
+    console.log("B: " + b);
+    console.log("C: " + c);
+}
+showNumber(1, 2, 3);
---
+++ Pack of exercises 1/EX004/index.ts
@@ -0,0 +1,7 @@
+function showNumber(a: number, b: number, c?: number) {
+	console.log("A: " + a);
+	console.log("B: " + b);
+	console.log("C: " + c);
+}
+
+showNumber(1, 2, 3);
