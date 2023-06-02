e79bc8a8e14d1ca795231915484d9f814df7822a
Nathan Mateus <117992913+onathanmateus@users.noreply.github.com>
2023-06-02 16:32:41 -0300

    Eu adicionei o .editorconfig

--- Pack of exercises 1/EX003/index.js
+++ Pack of exercises 1/EX003/index.js
@@ -1,6 +1,6 @@
 function passCordinates(cord) {
-  console.log("X cordinates: " + cord.x);
-  console.log("Y cordinates: " + cord.y);
+	console.log("X cordinates: " + cord.x);
+	console.log("Y cordinates: " + cord.y);
 }
 var objCord = { x: 323, y: 623 };
 passCordinates(objCord);
