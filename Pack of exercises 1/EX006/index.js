6b0097243c5fdd6d11435b998fc1ea21daf144c8
Nathan Mateus <117992913+onathanmateus@users.noreply.github.com>
2023-06-09 09:37:26 -0300

    Eu criei a pasta EX006

---
+++ Pack of exercises 1/EX006/index.js
@@ -0,0 +1,6 @@
+function showBalance(balance) {
+  console.log("O saldo da conta \u00E9 de R$".concat(balance));
+}
+showBalance(274627431);
+showBalance("1581742");
+var variados = [8372462, "Nathan", true];
---
+++ Pack of exercises 1/EX006/index.ts
@@ -0,0 +1,8 @@
+function showBalance(balance: string | number) {
+  console.log(`O saldo da conta é de R$${balance}`);
+}
+
+showBalance(274627431);
+showBalance("1581742");
+
+let variados: Array<number | string | boolean> = [8372462, "Nathan", true];
