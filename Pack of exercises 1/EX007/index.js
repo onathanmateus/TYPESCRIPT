59cc3055a026e017023809d6b72c1394620b6f72
Nathan Mateus <117992913+onathanmateus@users.noreply.github.com>
2023-06-22 09:46:39 -0300

    Eu criei a pasta EX007

---
+++ Pack of exercises 1/EX007/index.js
@@ -0,0 +1,6 @@
+var nome = "Nathan Mateus";
+var idade = 23;
+var peso = 63;
+var altura = 1.8;
+var imc = peso / Math.pow(altura, 2);
+console.log("Ol\u00E1, Meu nome \u00E9 ".concat(nome, ", eu tenho ").concat(idade, " anos e peso ").concat(peso, "KG, al\u00E9m disso eu tenho ").concat(altura, " metros de altura e o meu IMC \u00E9 de ").concat(imc, "."));
---
+++ Pack of exercises 1/EX007/index.ts
@@ -0,0 +1,9 @@
+const nome: string = "Nathan Mateus";
+const idade: number = 23;
+const peso: number = 63;
+const altura: number = 1.8;
+const imc: number = peso / Math.pow(altura, 2);
+console.log(
+  `Olá, Meu nome é ${nome}, eu tenho ${idade} anos e peso ${peso}KG, além disso 
+  eu tenho ${altura} metros de altura e o meu IMC é de ${imc}.`
+);
